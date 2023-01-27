b079e1ac445f8b511bacf29759d8c8b54189aaf8
wyclife12dube <wyclife12dube@gmail.com>
2023-01-27 01:35:06 -0800

    python classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""define a class Square."""
 
 class Square:
+    """Represent a square."""
 
     def __init__(self, size):
+
+        """initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
         self,__size = size
